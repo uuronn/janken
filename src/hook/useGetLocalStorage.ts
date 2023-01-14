054a24e1444f88df59380b1a125e309e3bc6e19b
uuronn <uuronprogram@gmail.com>
2023-01-15 02:46:03 +0900

    挙動テスト

--- src/hook/useGetLocalStorage.ts
+++ src/hook/useGetLocalStorage.ts
@@ -1,33 +1,30 @@
 import { useLayoutEffect, useState } from "react";
-import { LOCAL_STORAGE_KEY } from "~/data";
+import { LOCAL_MY_BEST_KEY } from "~/data";
 
 export const useGetLocalStorage = () => {
   const [localMyBest, setLocalMyBest] = useState<number>(0);
 
   useLayoutEffect(() => {
     const storage: number = JSON.parse(
-      localStorage.getItem(LOCAL_STORAGE_KEY) as string
+      localStorage.getItem(LOCAL_MY_BEST_KEY) as string
     );
 
-    if (storage === null) {
-      setLocalMyBest(0);
-      return;
-    }
+    if (storage === null) return;
 
     setLocalMyBest(storage);
   }, []);
 
   const localMyBestCounter = () => {
     if (!localMyBest) {
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(1));
+      localStorage.setItem(LOCAL_MY_BEST_KEY, JSON.stringify(1));
       return;
     }
 
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localMyBest + 1));
+    localStorage.setItem(LOCAL_MY_BEST_KEY, JSON.stringify(localMyBest + 1));
   };
 
   const localMyBestReset = () => {
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    localStorage.removeItem(LOCAL_MY_BEST_KEY);
   };
 
   return { localMyBest, localMyBestCounter, localMyBestReset };
